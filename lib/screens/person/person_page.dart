48227146df9c10ead7a5277b9d28c2cef0b54bc3
nzuwera <nzuwera2002@gmail.com>
2026-01-10 13:56:21 +0200

    Refactor PersonPage with enhanced ListView builder and update backend method usage

--- lib/screens/person/person_page.dart
+++ lib/screens/person/person_page.dart
@@ -10,7 +10,7 @@ class PersonPage extends StatefulWidget {
 }
 
 class _PersonPageState extends State<PersonPage> {
-   List<Person>? _personList;
+  List<Person>? _personList;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +26,7 @@ class _PersonPageState extends State<PersonPage> {
               minimumSize: Size(double.infinity, 32),
             ),
             onPressed: () async {
-              final persons = await BackendServices().fetchTexts();
+              final persons = await BackendServices().fetchPersons();
               setState(() {
                 _personList = persons;
               });
@@ -42,18 +42,32 @@ class _PersonPageState extends State<PersonPage> {
         SizedBox(height: 10),
         if (_personList != null && _personList!.isNotEmpty)
           Expanded(
-            child: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text("You have ${_personList!.length} Persons"),
-                ),
-                for (var person in _personList!)
-                  ListTile(
-                    leading: Icon(Icons.person),
-                    title: Text("${person.name} ${person.surname}"),
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: _personList!.length,
+              itemBuilder: (_, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    child: Text(
+                      _personList![index].name[0],
+                      style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary
+                      ),
+                    ),
                   ),
-              ],
+                  title: Text(
+                    _personList![index].name,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: Theme.of(context).colorScheme.primary
+                    ),
+                  ),
+                  subtitle: Text(
+                    "${_personList![index].name} ${_personList![index].surname}",
+                  ),
+                  trailing: Icon(Icons.delete_outlined),
+                );
+              },
             ),
           ),
       ],
